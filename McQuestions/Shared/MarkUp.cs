27d78bb48777ce5971803937f426dcb8de270f83
jules vannuijs <jules.vannuijs@bee-organisation.com>
2023-07-30 13:27:13 +0200

    Initial

---
+++ McQuestions/Shared/MarkUp.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace McQuestions.Shared
+{
+	public static class Extensions { public static MarkupString AsMarkup(this string s) => (MarkupString)s; }
+}
---
+++ McQuestions/Shared/RandomHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace McQuestions.Shared
+{
+	public static class RandomHelper
+	{
+		private static readonly Random random = new Random();
+
+		public static int Next(int minValue, int maxValue)
+		{
+			lock (random)
+			{
+				return random.Next(minValue, maxValue);
+			}
+		}
+	}
+
+}
