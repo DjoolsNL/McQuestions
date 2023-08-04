5fb08b7064b27bb01ae11dc126c6bdf7b43eb814
jules vannuijs <jules.vannuijs@bee-organisation.com>
2023-08-04 13:55:49 +0200

    Update

--- McQuestions/Shared/MarkUp.cs
+++ McQuestions/Shared/MarkUp.cs
@@ -1,6 +1,66 @@
 ﻿using Microsoft.AspNetCore.Components;
+using System.Xml.Linq;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace McQuestions.Shared
 {
-	public static class Extensions { public static MarkupString AsMarkup(this string s) => (MarkupString)s; }
+	public static class Extensions 
+	{
+		public static MarkupString AsMarkup(this string s)
+		{
+			return (MarkupString)s;
+		}
+	}
+
+	public class McObject 
+	{
+
+		public McObject(string filename) 
+		{
+			FileName = filename;
+			//Niveau = niveau;
+			filename = filename.Replace("{", "").Replace("}", "");
+            Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+            string fullpath = Path.Combine(Directory.GetCurrentDirectory() + "\\XmlQuestions\\", filename);
+            var xmlString = File.ReadAllText(fullpath);
+            Root = XElement.Parse(xmlString);
+            
+            NaamVragenSet = Root.Element("vragenset").Value;
+			
+        }
+
+		public  XElement Root { get; set; }
+
+        public  List<XElement>? XmlQuestionsLevelSpecified { get; set; }
+
+		public  string Niveau { get; set; }
+
+		public  string FileName { get; set; }
+
+		public  string NaamVragenSet { get; set; }
+
+		private int randomNumber;
+
+		public void SetLevelTestQuestions(string niveau)
+		{
+			Niveau = niveau;
+			var allEnabledQuestions = Root.Elements("vraag").Distinct().Where(e => e.Element("disabled").Value != "true").ToList();
+			XmlQuestionsLevelSpecified = allEnabledQuestions.FindAll(l => l.Element("niveau").Value == niveau);
+		}
+
+		public int TotalQuestionsLevelSpecified()
+		{
+			return XmlQuestionsLevelSpecified.Count();
+		}
+
+
+
+
+
+	}
+
+
 }
